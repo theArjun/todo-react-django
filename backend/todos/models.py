9a055b698f2abafdb1117b1e87753e046961aa83
thearjun <mailarjunadhikari@gmail.com>
2020-05-02 00:52:12 +0545

    Complete Backend

---
+++ backend/todos/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Todo(models.Model):
+
+    title = models.CharField(max_length=200)
+    body = models.TextField(default="")
+
+    def __str__(self):
+        return self.title
